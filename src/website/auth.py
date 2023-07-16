bee6778ed34a97b0e518e639ec6205043a61230d
mgelvoleo <mgelvoleo@gmail.com>
2023-07-16 17:33:25 +0800

    add flash messeges

--- src/website/auth.py
+++ src/website/auth.py
@@ -1,33 +1,34 @@
-from flask import Blueprint, redirect, url_for, render_template, request, url_for, session
+from flask import Blueprint, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
 
 auth = Blueprint("auth", __name__)
 
-auth.permanent_sessions_lifetime =  timedelta(minutes=5)
+auth.permanent_session_lifetime = timedelta(minutes=5)
 
-
-@auth.route("/login", methods=["POST","GET"])
+@auth.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-
         session.permanent = True
 
         fname = request.form["fname"].lower()
         lname = request.form["lname"].lower()
 
-
-        session["fname"] = fname 
+        session["fname"] = fname
         session["lname"] = lname
+
+        flash("Login Successful!")
 
         return redirect(url_for("auth.user"))
     else:
         if "fname" in session:
+            flash("Already Logged In!")
             return redirect(url_for("auth.user"))
-        
+
         return render_template("login.html")
-    
+
 @auth.route("/logout")
 def logout():
+    flash(f"You have been logged out", "info")
     session.pop("fname", None)
     return redirect(url_for("auth.login"))
 
@@ -35,6 +36,8 @@ def logout():
 def user():
     if "fname" in session:
         fname = session["fname"]
-        return f"<h1>{fname}</h1>"
-    else: 
+        flash(f"Welcome, {fname}!", "info")
+        return render_template("user.html", fname=fname)
+    else:
+        flash("You are not logged in")
         return redirect(url_for("auth.login"))
